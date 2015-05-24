d0ac49156f7f0c3f2c4d2e32bb5eb69f14ad6709
David Kuchar <davidkuchar@gmail.com>
2015-05-24 16:44:05 -0700

    talking to twitter oauth

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -8,22 +8,25 @@
 
 import UIKit
 
-let twitterBaseURLString = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterBaseURL")
-let twitterConsumerKey = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterConsumerKey")
-let twitterConsumerSecret = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterConsumerSecret")
-let twitterBaseURL = NSURL(string: twitterBaseURLString)
-
 class TwitterClient: BDBOAuth1RequestOperationManager {
    
     class func sharedInstance() -> TwitterClient {
         
         struct Static {
+            static let twitterBaseURL = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterBaseURL") as! String
+            static let twitterConsumerKey = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterConsumerKey") as! String
+            static let twitterConsumerSecret = NSBundle.mainBundle().objectForInfoDictionaryKey("TwitterConsumerSecret") as! String
+            
             static let instance = TwitterClient(
-                baseURL: twitterBaseURL,
+                baseURL: NSURL(string: twitterBaseURL),
                 consumerKey: twitterConsumerKey,
                 consumerSecret: twitterConsumerSecret
             )
         }
+//        
+//        println("TwitterConsumerKey \(Static.twitterConsumerKey)")
+//        println("TwitterConsumerSecret \(Static.twitterConsumerSecret)")
+        
         return Static.instance
     }
 }
