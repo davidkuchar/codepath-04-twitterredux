7edd0664d5f70343805dd074f85aa823e1bfb11a
David Kuchar <davidkuchar@gmail.com>
2015-05-24 17:00:19 -0700

    twitter login redirects back to app

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -27,9 +27,15 @@ class ViewController: UIViewController {
         
         TwitterClient.sharedInstance().requestSerializer.removeAccessToken()
         
-        TwitterClient.sharedInstance().fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+        TwitterClient.sharedInstance().fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cheep://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
 
                 println("Got the request token")
+            
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            
+            UIApplication.sharedApplication().openURL(authURL!)
+            
+            
             
             }) { (error: NSError!) -> Void in
             
